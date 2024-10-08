6ccbb9d731e0d31c3162abdd40bf1e6975cf34d5
JulyanaLozano <josephtemi@hotmail.com>
2024-10-08 17:11:58 -0500

    Actualizacion navbar

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,private router: Router) {}
 
   navigateTo(path: string) {
     this.router.navigate([path]);
+  }
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Aquí puedes agregar la lógica de autenticación
+      // Si la autenticación es exitosa, redirige al dashboard
+      this.router.navigate(['/dashboard']);
+    }
   }
 
 }
